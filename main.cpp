6b1de35ecde8485705d3aaed9df16c6cec39f160
AydenJulien <ayden.c.julien@gmail.com>
2025-08-14 20:37:21 +0930

    added file preface

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 // Assignment 1
-// input: I1 I2 B
+// 
+// input: I1 I2 B; all in 1 line, in presented format
 // I1 = inputNum1; can be from 0-100 digits long
 // I2 = inputNum2; can be from 1-100 digits long
-// B = base number of I1 and I2
-// output:
+// B = base number of I1 and I2; can be from 2-10
+// 
+// output: X Y Z; all in 1 line, in presented format
+// X = sum of I1 and I2; using school method
+// Y = product of I1 and I2; using Karatsuba algorithm
+// Z = ratio between I1 and I2; N/A; !! SET TO = 0 !!
 
-// !!All operations must take place in the base given; not converted
-// to an intermediary base, calculated, and then converted back!!
+// !! All operations must take place in the base given; not converted
+// to an intermediary base, calculated, and then converted back !!
 
+// potential note in lect2; 31:30
 
 
 int main() {
