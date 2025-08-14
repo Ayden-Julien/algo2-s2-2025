5380b53bfb886f39824a46f72549139cadacbea0
AydenJulien <ayden.c.julien@gmail.com>
2025-08-14 17:14:04 +0930

    basic file setup for main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// Assignment 1
+// input: I1 I2 B
+// I1 = inputNum1; can be from 0-100 digits long
+// I2 = inputNum2; can be from 1-100 digits long
+// B = base number of I1 and I2
+// output:
+
+// !!All operations must take place in the base given; not converted
+// to an intermediary base, calculated, and then converted back!!
+
+
+
+int main() {
+    
+
+    return;
+}
