c13ee56babd2d4259b1e564c84ac00f73a84623d
AydenJulien <ayden.c.julien@gmail.com>
2025-08-16 21:30:16 +0930

    made some progress and think i know what im doing for this addition

--- main.cpp
+++ main.cpp
@@ -17,16 +17,58 @@
 
 #include <string>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+// function for preforming school addition (Input1, Input2, Base)
+string schoolAddition (string Inp1, string Inp2, string B) {
+    // var setup
+    int inp1_length = Inp1.length();
+    int inp2_length = Inp2.length();
+    int max_length = max(inp1_length, inp2_length);
+
+    int inp1_digit = 0;
+    int inp2_digit = 0;
+
+    // actual addition
+    for (int i = 0; i < max_length; i++) {
+        // fetching the i-th digit of both inp1 and inp2
+        
+        // finding the i-th digit in input1, with error catcher
+        if (i < inp1_length) {
+            inp1_digit = Inp1[inp1_length - i - 1];
+        } 
+        else {
+            inp1_digit = 0;
+        }
+
+        // finding the i-th digit in input2, with error catcher
+        if (i < inp2_length) {
+            inp2_digit = Inp2[inp1_length - i - 1];
+        }
+        else {
+            inp2_digit = 0;
+        }
+
+        
+    }
+
+};
+
 int main() {
     // getting user input
-    string input;
-    cin >> input;
-    cout << endl;
+    string Input1; // input1
+    string Input2; // input2
+    string Base; // base
 
-    
+    cin >> Input1 >> Input2 >> Base;
+
+    string additionOutput = schoolAddition(Input1, Input2, Base);
+
+    // processing user input into something usable
+
+    // not sure if ^ is required
 
     return;
-}
+};
