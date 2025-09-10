3f82f3ad2f7d3dcaf4086569bc7c32adf3fbd167
AydenJulien <ayden.c.julien@gmail.com>
2025-09-10 09:57:45 +0930

    added explaination notes at top of file

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// start w/empty AVL tree
+// single input line of n modification moves
+// (1 <= n <= 100)
+// 
+// valid moves:
+// Aint; A followed by number (1 - 100); eg A3
+// means insert number; if in tree do nothing
+// Dint; D followed by number (1 - 100); eg D3
+// meand delete number; if not in tree do nothing
+//
+// input followed by (this is not included in the mod moves)
+// PRE; means output preordered tree
+// POST; means output ordered tree
+// IN; means output the input
+
+#include <string>
+#include <iostream>
+
+int main() {
+
+
+    return 0;
+}
