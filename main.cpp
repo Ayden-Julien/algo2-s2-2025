9ebe8c7a44f604401e0ef8d1ee82317c5025a9d5
AydenJulien <ayden.c.julien@gmail.com>
2025-09-13 09:58:59 +0930

    i dont think its in balance

--- main.cpp
+++ main.cpp
@@ -118,13 +118,13 @@ class AVL {
         // case left ___
         if (balance > 1) {
             // case left left sturcture bellow node
-            if (get_balance(node->left_branch) > 0) {
+            if (node->left_branch->left_branch != nullptr) {
                 node = rotate_right(node);
                 return node;
             }
 
             // case left right structure bellow node
-            else if (get_balance(node->right_branch) < 0) {
+            if (node->left_branch->right_branch != nullptr) {
                 node->left_branch = rotate_left(node->left_branch);
                 node = rotate_right(node);
                 return node;
@@ -134,13 +134,13 @@ class AVL {
         // case right ___
         if (balance < -1) {
             // case right right structure bellow node
-            if (get_balance(node->right_branch) < 0) {
+            if (node->right_branch->right_branch != nullptr) {
                 node = rotate_left(node);
                 return node;
             }
 
             // case right left sturture bellow node
-            else if (get_balance(node->left_branch) > 0) {
+            if (node->right_branch->left_branch != nullptr) {
                 node->right_branch = rotate_right(node->right_branch);
                 node = rotate_left(node);
                 return node;
