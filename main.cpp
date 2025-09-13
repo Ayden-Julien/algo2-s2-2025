6a77809e5f7fa7872de9b0a70f0c744301981fae
AydenJulien <ayden.c.julien@gmail.com>
2025-09-13 11:18:06 +0930

    maybe this fixed something, or maybe im thinking myself in circles cause im not keeping everything in my head

--- main.cpp
+++ main.cpp
@@ -108,48 +108,48 @@ class AVL {
     }
 
     // helper for balancing a tree
-    Node* balance_this(Node* node) {
-        int balance = get_balance(node);
+    //Node* balance_this(Node* node) {
+    //    int balance = get_balance(node);
         // recursively balancing up from new leaf
         // theoretiacl case where both secondary nodes 
         // != nullptr cannot exist due to balancing happening 
         // upwards
 
         // case left ___
-        if (balance > 1) {
+    //    if (balance > 1) {
             // case left left sturcture bellow node
-            if (node->left_branch->left_branch != nullptr) {
-                node = rotate_right(node);
-                return node;
-            }
+    //        if (node->left_branch->left_branch != nullptr) {
+    //            node = rotate_right(node);
+    //            return node;
+    //        }
 
             // case left right structure bellow node
-            if (node->left_branch->right_branch != nullptr) {
-                node->left_branch = rotate_left(node->left_branch);
-                node = rotate_right(node);
-                return node;
-            }
-        }
+    //        if (node->left_branch->right_branch != nullptr) {
+    //            node->left_branch = rotate_left(node->left_branch);
+    //            node = rotate_right(node);
+    //            return node;
+    //        }
+    //    }
 
         // case right ___
-        if (balance < -1) {
+    //    if (balance < -1) {
             // case right right structure bellow node
-            if (node->right_branch->right_branch != nullptr) {
-                node = rotate_left(node);
-                return node;
-            }
+    //        if (node->right_branch->right_branch != nullptr) {
+    //            node = rotate_left(node);
+    //            return node;
+    //        }
 
             // case right left sturture bellow node
-            if (node->right_branch->left_branch != nullptr) {
-                node->right_branch = rotate_right(node->right_branch);
-                node = rotate_left(node);
-                return node;
-            }
-        }
+    //        if (node->right_branch->left_branch != nullptr) {
+    //            node->right_branch = rotate_right(node->right_branch);
+    //            node = rotate_left(node);
+    //            return node;
+    //        }
+    //    }
 
         // if all else fails 
-        return node;
-    }
+    //    return node;
+    //}
 
     // insertion and deletion
     Node* private_insertion(Node* current_node, int num) {
@@ -176,10 +176,48 @@ class AVL {
         current_node->height = max(get_height(current_node->left_branch), get_height(current_node->right_branch)) + 1;
 
         // checking if insertion has unbalanced the tree
+        //int balance = get_balance(current_node);
+        //if (balance > 1 || balance < -1) {
+        //    current_node = balance_this(current_node);
+        //    return current_node;
+        //}
+
         int balance = get_balance(current_node);
-        if (balance > 1 || balance < -1) {
-            current_node = balance_this(current_node);
-            return current_node;
+        // recursively balancing up from new leaf
+        // theoretiacl case where both secondary nodes 
+        // != nullptr cannot exist due to balancing happening 
+        // upwards
+
+        // case left ___
+        if (balance > 1) {
+            // case left left sturcture bellow node
+            if (current_node->left_branch->left_branch != nullptr) {
+                current_node = rotate_right(current_node);
+                return current_node;
+            }
+
+            // case left right structure bellow node
+            if (current_node->left_branch->right_branch != nullptr) {
+                current_node->left_branch = rotate_left(current_node->left_branch);
+                current_node = rotate_right(current_node);
+                return current_node;
+            }
+        }
+
+        // case right ___
+        if (balance < -1) {
+            // case right right structure bellow node
+            if (current_node->right_branch->right_branch != nullptr) {
+                current_node = rotate_left(current_node);
+                return current_node;
+            }
+
+            // case right left sturture bellow node
+            if (current_node->right_branch->left_branch != nullptr) {
+                current_node->right_branch = rotate_right(current_node->right_branch);
+                current_node = rotate_left(current_node);
+                return current_node;
+            }
         }
 
         // case if insetion doesnt excede a balance of
@@ -251,9 +289,39 @@ class AVL {
 
         // if rebalancing is required once num is deleted
         int balance = get_balance(current_node);
-        if (balance > 1 || balance < -1) {
-            current_node = balance_this(current_node);
-            return current_node;
+        
+        // requires a different balancing system because deletion
+        // can happen anywhere in the tree, compared to insertion
+        // that can only happen at the leaves
+
+        // left bias
+        if (balance > 1) {
+            // left left
+            if (get_balance(current_node->left_branch) >= 0) {
+                current_node = rotate_right(current_node);
+                return current_node;
+            }
+            // left right
+            else if (get_balance(current_node->left_branch) < 0) {
+                current_node->left_branch = rotate_left(current_node->left_branch);
+                current_node = rotate_right(current_node);
+                return current_node;
+            }
+        }
+
+        // right bias
+        if (balance < -1) {
+            // right right
+            if (get_balance(current_node->right_branch) >= 0) {
+                current_node = rotate_left(current_node);
+                return current_node;
+            }
+            // right left
+            else if (get_balance(current_node->right_branch) < 0) {
+                current_node->right_branch = rotate_right(current_node->right_branch);
+                current_node = rotate_left(current_node);
+                return current_node;
+            }
         }
 
         // no rebalancing needed, backtracking to start
@@ -376,6 +444,11 @@ int main() {
         tree.indorder();
         cout << endl;
     }
+
+
+    // test inp:
+    // A88 D77 D95 A78 A71 A2 D9 A2 A60 D80 A85 A65 D11 A30 D8 A68 D87 A28 A88 A96 D29 D26 D88 D47 D68 A65 A86 A100 A61 D7 D76 D21 D24 A40 D94 A84 A16 D28 A45 A60 D34 D14 A68 A64 A74 A62 D99 D2 D34 D32 D60 D52 D19 A95 A28 A91 D24 A34 D22 D77 D7 D78 A3 A100 D95 D53 D82 D64 A55 A46 A17 A70 D4 A25 A75 D71 A30 D50 D44 A11 D39 A47 D77 A71 D1 A98 D51 A63 D15 A15 D75 A4 D14 D77 A9 D84 D70 A5 D67 A22 POST
+    // cout << "3 5 4 15 11 9 22 17 28 25 16 34 40 46 55 47 63 62 61 45 30 71 68 85 74 91 98 100 96 86 65" << endl;
 
     return 0;
 }
